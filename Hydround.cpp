38ada3a0b3e66f5d3ecf5a2ab1cad1a0cedece9c
undrfined <undrfined@gmail.com>
2016-02-17 09:00:18 +0200

    Hmm

--- Hydround.cpp
+++ Hydround.cpp
@@ -27,6 +27,36 @@ using namespace boost::asio;
 
 using namespace std;
 typedef unsigned char byte;
+std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems);
+std::vector<std::string> split(const std::string &s, char delim);
+int main(){
+	Translation t;
+	Log logger(t.getTranslation("server"));
+	Config cfg("Server.xml");
+	logger.info(t.getTranslation("loadingServer"));
+	int port = cfg.readInt("ServerPort", 19132);
+	char binding[32] = { };
+	sprintf(binding, t.getTranslation("binding").c_str(), port);
+	logger.info(string(binding));
+	io_service service;
+	char buff[1024];
+	ip::udp::socket sock(service, ip::udp::endpoint(ip::udp::v4(), port));
+	while (true) {
+		ip::udp::endpoint sender;
+		int bytes = sock.receive_from(buffer(buff), sender);
+		stringstream ss;
+		ss << buff;
+		std::vector<std::string> arr = split(ss.str(), 0x00);
+		long pingId = atol(arr[0].c_str());
+		ss.str("");
+		cout << pingId;
+		ss << 0x1c << pingId << 0x00 << 0xff << 0xff << 0x00 << 0xfe << 0xfe << 0xfe << 0xfe << 0xfd << 0xfd << 0xfd << 0xfd << 0x12 << 0x34 << 0x56 << 0x78 << "MCPE;§6§l » Hydround server §6«;41;0.14.0;0;10";
+		sock.send_to(buffer(ss.str()), sender);
+	}
+
+
+}
+
 std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
     std::stringstream ss(s);
     std::string item;
@@ -41,34 +71,4 @@ std::vector<std::string> split(const std::string &s, char delim) {
     std::vector<std::string> elems;
     split(s, delim, elems);
     return elems;
-}
-int main(){
-	Log logger(Translation->getTranslation("server"));
-	Config cfg("ServerConfig.json");
-	logger.info(Translation->getTranslation("loadingServer"));
-	int port = cfg.readInt("ServerPort", 19131);
-	char binding[32] = { };
-	sprintf(binding, t.getTranslation("binding", "Binding on port %d...").c_str(), port);
-	logger.info(string(binding));
-	io_service service;
-	char buff[1024];
-	ip::udp::socket sock(service, ip::udp::endpoint(ip::udp::v4(), port));
-	while (true) {
-		ip::udp::endpoint sender;
-		int bytes = sock.receive_from(buffer(buff), sender);
-		//Package incoming(buff[0], buff);
-		for (const char* p = buff; *p; ++p)
-		    {
-		        printf("%02x", *p);
-		    }
-		stringstream ss;
-		ss << buff;
-		std::vector<std::string> arr = split(ss.str(), 0x00);
-		long pingId = atol(arr[0].c_str());
-      ss.str("");//
-		ss << (char) 0x1c << pingId << (char) 0x00 << (char) 0xff << (char) 0xff << (char) 0x00 << (char) 0xfe << (char) 0xfe << (char) 0xfe << (char) 0xfe << (char) 0xfd << (char) 0xfd << (char) 0xfd << (char) 0xfd << (char) 0x12 << (char) 0x34 << (char) 0x56 << (char) 0x78 << "MCPE;§6§l » Hydround server §6«;41;0.14.0;0;10";
-		sock.send_to(buffer(ss.str()), sender);
-	}
-
-
 }
